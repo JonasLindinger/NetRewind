4ebbcfaebfd7f35d8fb135b7ea8fecd5489478d9
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2025-08-29 18:47:14 +0200

    created NetworkRunner scripts

---
+++ Assets/NetRewind/NetworkRunner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+
+namespace NetRewind
+{
+    [RequireComponent(typeof(NetworkManager))]
+    [RequireComponent(typeof(UnityTransport))]
+    public class NetworkRunner : MonoBehaviour
+    {
+        public static NetworkRunner Runner { get; private set; }
+
+        private void Awake()
+        {
+            // Ensure only one instance of NetworkRunner exists
+            if (Runner == null)
+            {
+                Runner = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Debug.LogError("There can only be one NetworkRunner!");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
