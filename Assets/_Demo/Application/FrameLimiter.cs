3807d66cdfc08866afd4637aa9bed39c06d77312
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2025-08-29 19:05:50 +0200

    add: Scene Loading and Frame Limiter

---
+++ Assets/_Demo/Application/FrameLimiter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace NetRewind.Demo.Application
+{
+    public static class FrameLimiter
+    {
+        private static uint targetFrameRate = 240;
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void Init()
+        {
+            UnityEngine.Application.targetFrameRate = (int) targetFrameRate;
+        }
+    }
+}
---
+++ Assets/_Demo/Scene/AutomaticSceneSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using SceneManagement;
+using UnityEngine;
+
+namespace _Demo.Scene
+{
+    public class AutomaticSceneSwitcher : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField] private uint sceneGroupIndexToLoad = 0;
+
+        private async void Start()
+        {
+            await SceneLoader.GetInstance().LoadSceneGroup((int) sceneGroupIndexToLoad);
+        }
+    }
+}
