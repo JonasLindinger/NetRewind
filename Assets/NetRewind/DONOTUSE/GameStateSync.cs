c9439d68d90add2480c5df9b1d40968d5c7be7bb
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2025-09-02 18:22:47 +0200

    add static foreach in static gamestatesync on state tick

--- Assets/NetRewind/DONOTUSE/GameStateSync.cs
+++ Assets/NetRewind/DONOTUSE/GameStateSync.cs
@@ -1,15 +1,39 @@
-﻿using Unity.Netcode;
+﻿using System.Collections.Generic;
+using Unity.Netcode;
 
 namespace NetRewind.DONOTUSE
 {
     public class GameStateSync : NetworkBehaviour
     {
+        #if Client
         public static uint latestReceavedServerGameStateTick; // Todo: set this
+        #endif
         
+        #if Server
+        private static List<GameStateSync> clients = new List<GameStateSync>();
+        #endif
+
+        public override void OnNetworkSpawn()
+        {
+            #if Server
+            clients.Add(this);
+            #endif
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            #if Server
+            clients.Remove(this);
+            #endif
+        }
+
         #if Server
         public static void SendGameState(uint _) 
         {
-            
+            foreach (var client in clients)
+            {
+                
+            }
         }
         #endif
     }
